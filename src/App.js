6a4b6a7fa7b7991cfae260e20adcaa86ed441b7c
JihadQade <jihadkade@gmail.com>
2024-08-21 23:47:31 +0300

    asd

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe
+          src="https://taplink.cc/helpme1"
+          title="WebView"
+          style={{
+            width: '100%',
+            height: '100vh',
+            border: 'none',
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
